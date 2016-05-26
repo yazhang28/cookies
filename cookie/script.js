3b1d649e7490ae518151f4cbe69475d1063cf154
yazhang28 <yazhang@bu.edu>
2016-05-25 21:07:46 -0400

    condensed cookieAnim

--- cookie/script.js
+++ cookie/script.js
@@ -1,25 +1,26 @@
-var cookieId = document.getElementById("cookie");
 var enterBtnId = document.getElementById("enter_btn");
 var landPageId = document.getElementById("landingPage");
-
 		
 enterBtnId.addEventListener("click", function() {displayHidden(landPageId)});
-enterBtnId.addEventListener("mouseover", cookieAnimate);
-enterBtnId.addEventListener("mouseout", cookieAnimate2);
+enterBtnId.addEventListener("mouseover", function() {cookieAnim("translate(0, -10px)", "0.5s", "", "cubic-bezier(.35,.84,.62,.79)")})
+enterBtnId.addEventListener("mouseout", function() {cookieAnim("translate(0, 0)", "0.5s", "0.1s", "cubic-bezier(.35,.84,.62,.79)")})
 
 function displayHidden(element) {
 	element.style.display = "block";
 }
 
-function cookieAnimate() {
-	cookieId.style.transform = "translate(0, -10px)";
-	cookieId.style.transition = "0.5s";
-	cookieId.style.transitionTimingFunction = "cubic-bezier(.35,.84,.62,.79)";
-}
+function cookieAnim(transf, transi, transDelay, transFunction) {
+	var cookieId = document.getElementById("cookie");
+	cookieId.style.transform = transf;
+	cookieId.style.msTransform = transf; // IE
+	cookieId.style.WebkitTransform = transf; // Chrome & Safari
 
-function cookieAnimate2() {
-	cookieId.style.transform = "translate(0, 0)";
-	cookieId.style.transition = "0.5s";
-	cookieId.style.transitionDelay = "0.1s";
-	cookieId.style.transitionTimingFunction = "cubic-bezier(.35,.84,.62,.79)";	
+	cookieId.style.transition = transi;
+	cookieId.style.WebkitTransition = transi;
+
+	cookieId.style.transitionDelay = transDelay;
+	cookieId.style.WebkitTransitionDelay = transDelay;
+
+	cookieId.style.transitionTimingFunction = transFunction;	
+	cookieId.style.WebkitTransitionTimingFunction = transFunction;	
 }
