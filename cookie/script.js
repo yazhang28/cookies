eda2a0ecde692b1b525a95a182ec0abd4ded90fd
yazhang28 <yazhang@bu.edu>
2016-05-28 18:27:17 -0400

    now about shows after enter animation completes

--- cookie/script.js
+++ cookie/script.js
@@ -1,5 +1,10 @@
 var enterBtnId = document.getElementById("enter_btn");
 var cookieId = document.getElementById("cookie");
+var welcomeScreenId = document.getElementById("welcomeScreen");
+
+welcomeScreenId.addEventListener("webkitTransitionEnd", showAbout);
+welcomeScreenId.addEventListener("mozTransitionEnd", showAbout);
+welcomeScreenId.addEventListener("oTransitionEnd", showAbout);
 enterBtnId.addEventListener("click", function() {closeWelcome()});
 enterBtnId.addEventListener("mouseover",
 	function() {cookieAnim("translate(0, -15px)",
@@ -13,15 +18,39 @@ enterBtnId.addEventListener("mouseout",
 		"0.1s",
 		"cubic-bezier(.35,.84,.62,.79)")})
 
+function showAbout() {
+	var aboutContentId = document.getElementById("about-content");
+	var aboutHeadId = document.getElementById("about-head");
+
+	aboutContentId.style.marginTop = "20px";
+	aboutHeadId.style.marginTop = "20px";
+	aboutContentId.style.opacity = "1";
+	aboutHeadId.style.opacity = "1";
+
+	aboutHeadId.style.transition = "opacity 1s, margin-top 1s";
+	aboutHeadId.style.webkitTransition = "opacity 1s, margin-top 1s";
+
+	aboutContentId.style.webkitTransition = "opacity 1s, margin-top 1s";
+	aboutContentId.style.transition = "opacity 1s, margin-top 1s";
+
+	aboutContentId.style.webkitTransitionDelay = "0.5s";
+	aboutContentId.style.transitionDelay = "0.5s";
+
+}
+
 function closeWelcome() {
-	document.getElementById("welcomeScreen").style.height = "0%";
+	welcomeScreenId.style.height = "0%";
 	enterBtnId.style.opacity = "0";
 	cookieId.style.opacity = "0";
-	cookieId.style.transition = "opacity 0.5s";
-	cookieId.style.WebkitTransition = "opacity 0.5s";
-	enterBtnId.style.transition = "opacity 0.5s";
-	enterBtnId.style.WebkitTransition = "opacity 0.5s";
-	
+
+	welcomeScreenId.style.transition = " height 1s"
+	welcomeScreenId.style.WebkitTransition = "height 1s";
+
+	cookieId.style.transition = "opacity 0.7s";
+	cookieId.style.WebkitTransition = "opacity 0.7s";
+
+	enterBtnId.style.transition = "opacity 0.7s";
+	enterBtnId.style.WebkitTransition = "opacity 0.7s";
 }
 
 function cookieAnim(transf, transi, transDelay, transFunction) {
