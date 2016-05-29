7535c865b40063afc4381b61c219c4fd6863c210
yazhang28 <yazhang@bu.edu>
2016-05-28 20:43:06 -0400

    scrolling on welcome page now disabled

--- cookie/script.js
+++ cookie/script.js
@@ -2,16 +2,22 @@ var enterBtnId = document.getElementById("enter_btn");
 var cookieId = document.getElementById("cookie");
 var welcomeScreenId = document.getElementById("welcomeScreen");
 
+// show about after welcome page transitions
 welcomeScreenId.addEventListener("webkitTransitionEnd", showAbout);
 welcomeScreenId.addEventListener("mozTransitionEnd", showAbout);
 welcomeScreenId.addEventListener("oTransitionEnd", showAbout);
+
+// closes welcome page when enter button clicked
 enterBtnId.addEventListener("click", function() {closeWelcome()});
+
+// cookie transition when hovering over enter button
 enterBtnId.addEventListener("mouseover",
 	function() {cookieAnim("translate(0, -15px)",
 		"0.5s",
 		"", 
 		"cubic-bezier(.35,.84,.62,.79)")})
 
+// cookie transition when mouse moves away from enter button
 enterBtnId.addEventListener("mouseout", 
 	function() {cookieAnim("translate(0, 0)",
 		"0.5s",
@@ -30,15 +36,20 @@ function showAbout() {
 	aboutHeadId.style.transition = "opacity 1s, margin-top 1s";
 	aboutHeadId.style.webkitTransition = "opacity 1s, margin-top 1s";
 
+	aboutHeadId.style.webkitTransitionDelay = "0.3s";
+	aboutHeadId.style.transitionDelay = "0.3s";
+
 	aboutContentId.style.webkitTransition = "opacity 1s, margin-top 1s";
 	aboutContentId.style.transition = "opacity 1s, margin-top 1s";
 
-	aboutContentId.style.webkitTransitionDelay = "0.5s";
-	aboutContentId.style.transitionDelay = "0.5s";
+	aboutContentId.style.webkitTransitionDelay = "0.7s";
+	aboutContentId.style.transitionDelay = "0.7s";
 
 }
 
+// closes welcome screen and re-enable scroll
 function closeWelcome() {
+	document.body.style.overflow = "auto";
 	welcomeScreenId.style.height = "0%";
 	enterBtnId.style.opacity = "0";
 	cookieId.style.opacity = "0";
