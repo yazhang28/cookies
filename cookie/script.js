aa5f2d3506eb230c114f6b7696ca10373c337343
yazhang28 <yazhang@bu.edu>
2016-05-31 18:26:01 -0400

    fixed about showing when hovering over enter button

--- cookie/script.js
+++ cookie/script.js
@@ -2,28 +2,8 @@ var enterBtnId = document.getElementById("enter-btn");
 var cookieId = document.getElementById("cookie");
 var welcomeScreenId = document.getElementById("welcomeScreen-init");
 var wrapAboutId = document.getElementById("wrap-about");
-var aboutId = document.getElementById("about");
-var aboutContentId = document.getElementById("about-content-init");
 var aboutHeadId = document.getElementById("about-head-init");
-
-window.addEventListener("scroll", divPassed);
-
-function divPassed() {
-	var bottomLeft = wrapAboutId.clientHeight + wrapAboutId.clientTop;
-	if (scrollY > bottomLeft) {
-		console.log("passed");
-		aboutId.style.opacity = "0";
-		aboutId.style.transition = "0.1s";
-	} else {
-		console.log("not passed");
-		aboutId.style.opacity = "1";
-		aboutId.style.transition = "1s";
-	}
-}
-
-function hideElement(element) {
-	element.style.opacity = "0";
-}
+var aboutContentId = document.getElementById("about-content-init");
 
 // animate cookie icon when mousing over enter button
 enterBtnId.addEventListener("mouseover", function () { cookieAnim("cookie-anim")});
@@ -32,10 +12,8 @@ enterBtnId.addEventListener("mouseout", function () {cookieAnim("cookie")});
 // closes welcome screen and re-enable scroll when enter button clicked
 enterBtnId.addEventListener("click", function() {closeWelcome()});
 
-// show about after welcome page transitions
-welcomeScreenId.addEventListener("webkitTransitionEnd", showAbout);
-welcomeScreenId.addEventListener("mozTransitionEnd", showAbout);
-welcomeScreenId.addEventListener("oTransitionEnd", showAbout);
+// check if element is in viewport
+window.addEventListener("scroll", divPassed);
 
 function cookieAnim(setId) {
 	cookieId.id = setId;
@@ -46,9 +24,23 @@ function closeWelcome() {
 	welcomeScreenId.id = "welcomeScreen-final";
 	enterBtnId.id = "enter-btn-final";
 	cookieId.id = "cookie-final";
+	setTimeout(showAbout, 500);
 }
 
 function showAbout() {
-	aboutContentId.id = "about-content-final";
-	aboutHeadId.id = "about-head-final";
+		aboutHeadId.id = "about-head-final";
+		aboutContentId.id = "about-content-final";
+}
+
+// make function usable for all elements
+function divPassed() {
+	var bottomLeft = wrapAboutId.clientHeight + wrapAboutId.clientTop;
+	if (scrollY > bottomLeft) {
+		console.log("passed");
+		aboutHeadId.id = "about-head-init";
+		aboutContentId.id = "about-content-init";
+	} else {
+		console.log("not passed");
+		showAbout();
+	}
 }
