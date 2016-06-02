5f7c82d3b715fbf0919f1575a1f3f28e7309249f
yazhang28 <yazhang@bu.edu>
2016-06-01 21:22:47 -0400

    Add files via upload
    need to center, add ease-in in animation when user stops scrolling, normally button hidden.

--- cookie/script.js
+++ cookie/script.js
@@ -5,7 +5,7 @@ var wrapAboutId = document.getElementById("wrap-about");
 var aboutHeadId = document.getElementById("aboutHead-init");
 var aboutContentId = document.getElementById("aboutContent-init");
 var pullTabIconId = document.getElementById("pullTabBtnIcon");
-var pullTabBtnId = document.getElementById("pullTabBtn");
+var pullTabBtnId = document.getElementById("pullTabBtn-final");
 
 // animate cookie icon when mousing over enter button
 enterBtnId.addEventListener("mouseover", function () {
@@ -18,24 +18,34 @@ enterBtnId.addEventListener("mouseout", function () {
 
 // closes welcome screen and re-enable scroll when enter button clicked
 enterBtnId.addEventListener("click", function() {
-	pullTabIconId.className = "glyphicon glyphicon-chevron-up img-responsive";
+	window.scrollTo(0,0);
+	aboutAction("hide");
+	pullTabIconId.className = "glyphicon glyphicon-remove img-responsive";
 	setTimeout(function () {
 		welcomeAction("open");
+		pullTabBtnId.id = "pullTabBtn";
 	}, 300);
 });
 
 // return to welcome screen and disable scroll when tab is clicked
 pullTabBtnId.addEventListener("click", function () {
-	pullTabIconId.className = "glyphicon glyphicon-remove img-responsive";
+	pullTabIconId.className = "glyphicon glyphicon-chevron-up img-responsive";
 	setTimeout(function () {
 		welcomeAction("close");
-		hideAbout();
+		pullTabBtnId.id = "pullTabBtn-final";
 	}, 300);
+	setTimeout(function () {
+		window.scrollTo(0,0);
+		aboutAction("hide");
+	}, 1000);
 });
 
 
 // check if element is in viewport
-window.addEventListener("scroll", divPassed);
+window.addEventListener("scroll", function () {
+	
+	divPassed();
+});
 
 function cookieAnim(setId) {
 	cookieId.id = setId;
@@ -47,7 +57,10 @@ function welcomeAction(action) {
 		welcomeScreenId.id = "welcomeScreen-final";
 		enterBtnId.id = "enterBtn-final";
 		cookieId.id = "cookie-final";
-		setTimeout(showAbout, 500);
+		setTimeout(function () {
+			aboutAction("show")
+		}, 500);
+
 	} else {
 		document.body.style.overflow = "hidden";
 		welcomeScreenId.id = "welcomeScreen-init";
@@ -56,24 +69,22 @@ function welcomeAction(action) {
 	}
 }
 
-function showAbout() {
+function aboutAction(action) {
+	if (action == "hide") {
+		aboutHeadId.id = "aboutHead-init";
+		aboutContentId.id = "aboutContent-init";
+	} else {
 		aboutHeadId.id = "aboutHead-final";
 		aboutContentId.id = "aboutContent-final";
-}
-
-function hideAbout() {
-	aboutHeadId.id = "aboutHead-init";
-	aboutContentId.id = "aboutContent-init";
+	}
 }
 
 // make function usable for all elements
 function divPassed() {
 	var bottomLeft = wrapAboutId.clientHeight + wrapAboutId.clientTop;
 	if (scrollY > bottomLeft) {
-		// aboutHeadId.id = "aboutHead-init";
-		// aboutContentId.id = "aboutContent-init";
-		hideAbout();
+		aboutAction("hide")
 	} else {
-		showAbout();
+		aboutAction("show");
 	}
 }
